4795cc79453aa35cd677e6932a8d776f1223874b
dayal.murukutla <dayal.murukutla@target.com>
2018-02-19 11:52:42 -0600

    Add SinglyLinkedList and DoublyLinkedList based implementations

--- BarrenLandAnalyzer.swift
+++ BarrenLandAnalyzer.swift
@@ -10,8 +10,7 @@ import Foundation
 protocol Queue {
     associatedtype Element
     func enqueue(_ element:Element)
-    func dequeue()->Element
-    func size()->Int
+    func dequeue()->Element?
     var isEmpty:Bool {get}
 }
 
@@ -21,16 +20,131 @@ class SimpleQueue<T>:Queue{
     func enqueue(_ element:T){
         elements.append(element)
     }
-    func dequeue()->T {
+    func dequeue()->T? {
+        
+        guard !self.isEmpty else {
+            return nil
+        }
+        
         return elements.removeFirst()
-    }
-    
-    func size()->Int {
-        return elements.count
     }
     
     var isEmpty:Bool {
         return elements.isEmpty
+    }
+}
+
+class DoublyLinkedList<T>:Queue {
+    var head:Node<T>?
+    var tail:Node<T>?
+    
+    typealias Element = T
+    
+    func enqueue(_ element:T) {
+        let node = Node(element)
+        
+        if head == nil { // head is not set
+            self.head = node
+        } else if tail == nil { // head is set but tail is not
+            self.head?.nextNode = node
+            self.tail = node
+            node.previousNode = self.head
+            
+        } else { // both head and tail are set
+            node.previousNode = self.tail
+            self.tail?.nextNode = node
+            self.tail = node
+        }
+        
+    }
+    
+    func dequeue()->T? {
+        if self.head == nil {
+            return nil
+        }
+        let nodeForDequeue = self.head
+        let nextHead = self.head?.nextNode
+        self.head = nextHead
+        
+        if self.head == nil || self.head?.nextNode == nil {
+            self.tail = nil
+        }
+        
+        return nodeForDequeue?.value
+    }
+    
+    var isEmpty:Bool {
+        return self.head == nil
+    }
+    
+}
+
+class SinglyLinkedList<T>:Queue {
+    var head:SinglyLinkedNode<T>?
+    var tail:SinglyLinkedNode<T>?
+    
+    typealias Element = T
+    
+    func enqueue(_ element:T) {
+        let node = SinglyLinkedNode(element)
+        
+        if head == nil { // head is not set
+            self.head = node
+        } else if tail == nil { // head is set but tail is not set
+            self.head?.nextNode = node
+            self.tail = node
+        } else { // head is set tail is set
+            self.tail?.nextNode = node
+            self.tail = node
+        }
+    }
+    
+    func dequeue()->T? {
+        
+        guard let head = self.head else {
+            return nil
+        }
+        
+        let value = head.value
+        
+        let nextHead = head.nextNode
+        
+        self.head = nextHead
+        
+        if self.head == nil {
+            self.tail = nil
+        }
+        
+        return value
+    }
+    
+    var isEmpty:Bool {
+        return self.head == nil
+    }
+}
+
+
+class SinglyLinkedNode<T> {
+    let value:T
+    var nextNode:SinglyLinkedNode<T>?
+    init(_ value:T) {
+        self.value = value
+    }
+    deinit {
+        //print("SinglyLinkedNode with value \(self.value) will be deinitialized")
+    }
+}
+
+class Node<T>{
+    let value:T
+    weak var previousNode:Node<T>?
+    var nextNode:Node<T>?
+    init(_ value:T) {
+        self.value = value
+    }
+    
+    deinit {
+        //print("Node with value \(self.value) will be deinitialized")
     }
 }
 
@@ -80,7 +194,12 @@ class BarrenLandAnalyzer {
         var x = 0
         var y = 0
         var currentSection = 0
-        let queue = SimpleQueue<Location>()
+        
+        //Best performance is with a SinglyLinkedList, followed by the Array backed SimpleQueue and then followed closely with a DoublyLinkedList
+        //let queue = SimpleQueue<Location>()
+        //let queue = DoublyLinkedList<Location>()
+        let queue = SinglyLinkedList<Location>()
+        
         var parcelMap:[Int:Int] = [:]
         while x < WIDTH && y < HEIGHT {
             if queue.isEmpty {
@@ -99,7 +218,7 @@ class BarrenLandAnalyzer {
             }
             
             if !queue.isEmpty {
-                let location = queue.dequeue()
+                let location = queue.dequeue()!
                 
                 if !isVisited(location: location, farm: farm) {
                     parcelMap[currentSection] = parcelMap[currentSection]! + 1
